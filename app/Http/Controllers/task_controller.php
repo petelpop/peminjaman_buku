37b9160a43adeed9e29d283621f08f530de865b1
petelpop <m.naufal.s.k@gmail.com>
2023-02-13 10:56:29 +0700

    migrate

---
+++ app/Http/Controllers/task_controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class task_controller extends Controller
+{
+    //
+}
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('main')
+
+<div>
+  <table class="table table-striped-columns">
+    ...
+  </table>
+</div>
+    
+@endsection
