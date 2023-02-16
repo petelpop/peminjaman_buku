e2dce64e3de02e879bf03363975248e858d72be6
petelpop <m.naufal.s.k@gmail.com>
2023-02-16 14:29:37 +0700

    create forgot password function

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -50,5 +50,13 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.login');
         });
 
+        Fortify::requestPasswordResetLinkView(function () {
+            return view('auth.forgot-password');
+        });
+
+        Fortify::resetPasswordView(function($request){
+            return view('auth.reset-password', ['request' => $request]);
+        });
+
     }
 }
